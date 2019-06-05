1e71c4be3c48c7e1247c38b5ac6fb47ce670d99b
Victor Gonzalez <victorgonzalez.r1998@gmail.com>
2019-06-05 10:46:32 +0200

    Creating physics

--- pyGame.py
+++ pyGame.py
@@ -1,68 +1,65 @@
-import pygame, sys
+import pygame, sys, math
 from pygame.locals import *
 
-BLUE = (108, 226, 240)
-RED = (244, 128, 66)
+# import Box2D # The main library
+from Box2D import *
+
+# --- constants ---
+TARGET_FPS = 60
+TIME_STEP = 1.0 /TARGET_FPS
+screenXY = SCREEN_WIDTH, SCREEN_HEIGHT = 1140, 480
 GREEN = (116, 255, 51)
-HEIGTH = 1000
-WIDTH = 1800
-DISPLAY = pygame.display.set_mode((WIDTH, HEIGTH))
-class Player:
-    def __init__(self):
-        self.circle_radius = 50
-        self.wheel_distance = 100 + 2*self.circle_radius
-        self.wheel1_x = self.circle_radius
-        self.wheel1_y = int(HEIGTH - HEIGTH/5)-self.circle_radius
-        self.wheel2_x = self.circle_radius + self.wheel_distance
-        self.wheel2_y = int(HEIGTH - HEIGTH/5)-self.circle_radius
+vel_iters, pos_iters = 6, 2
 
-    def initialPosition(self):
-        pygame.draw.circle(DISPLAY, RED, (self.wheel1_x, self.wheel1_y), self.circle_radius)
-        pygame.draw.circle(DISPLAY, RED, (self.wheel2_x, self.wheel2_y), self.circle_radius)
+# --- pygame setup ---
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+pygame.display.set_caption('Simple pygame example')
+world = b2World(gravity=(0.0, 5.0), doSleep=True)
+clock = pygame.time.Clock()
 
-    def movement(self):
-        keypressed = pygame.key.get_pressed()
-        if keypressed[K_LEFT] and self.wheel1_x - 1 >= self.circle_radius:
-            self.wheel1_x = self.wheel1_x - 2
-            self.wheel2_x = self.wheel2_x - 2
-            pygame.draw.circle(DISPLAY, RED, (self.wheel1_x, self.wheel1_y), self.circle_radius)
-            pygame.draw.circle(DISPLAY, RED, (self.wheel2_x, self.wheel2_y), self.circle_radius)
+groundBodyDef = b2BodyDef()
+groundBodyDef.position = (0, 200)
 
-        if keypressed[K_RIGHT] and self.wheel2_x + 1 <= self.WIDTH - self.circle_radius:
-            self.wheel1_x = self.wheel1_x + 2
-            self.wheel2_x = self.wheel2_x + 2
-            pygame.draw.circle(DISPLAY, RED, (self.wheel1_x, self.wheel1_y), self.circle_radius)
-            pygame.draw.circle(DISPLAY, RED, (self.wheel2_x, self.wheel2_y), self.circle_radius)
+groundBody = world.CreateBody(groundBodyDef)
+
+groundBox = b2PolygonShape(box=(SCREEN_WIDTH, 10))
+
+groundBoxFixture = b2FixtureDef(shape=groundBox)
+
+groundBody.CreateFixture(groundBoxFixture)
 
 
-class Board:
-
-    def create(self):
-        pygame.draw.rect(DISPLAY, GREEN, (0,HEIGTH - HEIGTH/5, WIDTH, HEIGTH/5))
-        pygame.draw.rect(DISPLAY, BLUE, (0,0, WIDTH,4*HEIGTH/5),0)
+body = world.CreateDynamicBody(position=(100,0))
+body.mass = 10
 
 
-    def refresh(self):
-        pygame.draw.rect(DISPLAY, BLUE, (0,0, WIDTH,4*HEIGTH/5))
 
-class GAME():
-
-    def main_loop(self):
-        pygame.init()
-        player = Player()
-        board = Board()
-        board.create()
-        player.initialPosition()
-        while True:
-            board.refresh()
-            player.movement()
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-            pygame.display.update()
+box = body.CreatePolygonFixture(box=(10, 10), density=1,friction=0.09)
 
 
-if __name__ == '__main__':
-    x = GAME()
-    x.main_loop()
+
+while True:
+    keypressed = pygame.key.get_pressed()
+    if keypressed[K_RIGHT]:
+        body.ApplyForce(force=(600,0),point=(body.position[0],body.position[1]),wake=True)
+    if keypressed[K_LEFT]:
+        body.ApplyForce(force=(-600,0),point=(body.position[0],body.position[1]),wake=True)
+    if keypressed[K_SPACE]:
+        body.ApplyForce(force=(0,-9999),point=(body.position[0],body.position[1]),wake=True)
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, GREEN, (groundBody.position[0],groundBody.position[1],SCREEN_WIDTH,10*2))
+    pygame.draw.rect(screen, GREEN, (math.ceil(body.position[0]),math.ceil(body.position[1]),10,10*2))
+    #print(body.position, body.angle)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            clock.tick(TARGET_FPS)
+            print('Done!')
+            pygame.quit()
+            sys.exit()
+    world.Step(TIME_STEP, vel_iters, pos_iters)
+    world.ClearForces()
+    pygame.display.flip()
+
+
+
